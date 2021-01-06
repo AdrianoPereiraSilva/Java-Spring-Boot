f0dfcd8320b2f3f9037814f6bea58ebe3e13a652
Adriano Silva <apesilva2012@gmail.com>
2021-01-06 18:14:27 -0300

    Adding SpringBoot Support

--- springboot-maven/src/main/java/br/com/project/springboot/Startup.java
+++ springboot-maven/src/main/java/br/com/project/springboot/Startup.java
@@ -1,7 +1,12 @@
 package br.com.project.springboot;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Startup {
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		SpringApplication.run(Startup.class);
 	}
 }
