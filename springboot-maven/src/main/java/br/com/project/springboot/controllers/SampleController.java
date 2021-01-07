cbf16af5d4a4090cfb61e8a45758f69f13510444
Adriano Silva <apesilva2012@gmail.com>
2021-01-07 18:30:14 -0300

    Create a SimpleController for tests

---
+++ springboot-maven/src/main/java/br/com/project/springboot/controllers/SampleController.java
@@ -0,0 +1,16 @@
+package br.com.project.springboot.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RestController
+public class SampleController {
+
+	@GetMapping("/hello-world")
+	public String helloWorld() {	
+		return "Hello World";
+	}
+
+}
