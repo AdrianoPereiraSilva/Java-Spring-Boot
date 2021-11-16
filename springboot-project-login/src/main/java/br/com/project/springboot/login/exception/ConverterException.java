b66287d5092bac04b92922c03feece48bfb30837
AdrianoPereiraSilva <apesilva2012@gmail.com>
2021-11-15 22:11:43 -0300

    Adicionada a classe ConverterException para erros de conversão no DozerConverter

---
+++ springboot-project-login/src/main/java/br/com/project/springboot/login/exception/ConverterException.java
@@ -0,0 +1,9 @@
+package br.com.project.springboot.login.exception;
+
+public class ConverterException extends RuntimeException{
+
+    public ConverterException (String message) {
+        super(message);
+    }
+
+}
