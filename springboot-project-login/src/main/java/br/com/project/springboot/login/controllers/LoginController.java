9fc80d387986ee5b5a9ae8ad51676e076cbfcd67
AdrianoPereiraSilva <apesilva2012@gmail.com>
2021-11-17 08:23:15 -0300

    Refactor na estrutura do Service criando uma Interface para controle de métodos a serem implementados e abstração

--- springboot-project-login/src/main/java/br/com/project/springboot/login/controllers/LoginController.java
+++ springboot-project-login/src/main/java/br/com/project/springboot/login/controllers/LoginController.java
@@ -1,10 +1,7 @@
 package br.com.project.springboot.login.controllers;
 
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
-
+import br.com.project.springboot.login.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.project.springboot.login.data.vo.UserVO;
-import br.com.project.springboot.login.services.LoginService;
+import br.com.project.springboot.login.services.impl.LoginServiceImpl;
 
 @RestController
 @RequestMapping("/api/login/v1")
